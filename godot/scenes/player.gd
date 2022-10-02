7ac584cb10b349ab97b222f6c11b6900434ad537
rzuf <rzuf79@gmail.com>
2022-10-02 15:45:18 +0200

    knur flying around

---
+++ godot/scenes/player.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+export(float) var speed = 300.0
+
+onready var _sprite = $"%Sprite"
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("up"):
+		direction.y = -1
+	if Input.is_action_pressed("down"):
+		direction.y = 1
+	if Input.is_action_pressed("left"):
+		direction.x = -1
+	if Input.is_action_pressed("right"):
+		direction.x = 1
+	
+	position += direction * speed * delta
